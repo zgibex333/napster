e041c6ff781e054f757711ccff8b93cef7c1f3f6
Dzianis Chaplia <zgibex333@gmail.com>
2023-09-03 13:35:14 +0300

    Layout, Header, Sidebar

---
+++ components/Sidebar.tsx
@@ -0,0 +1,54 @@
+"use client";
+import { usePathname } from "next/navigation";
+import { useMemo } from "react";
+import { HiHome } from "react-icons/hi";
+import { BiSearch } from "react-icons/bi";
+import Box from "@/components/Box";
+import SidebarItem from "@/components/SidebarItem";
+import Library from "@/components/Library";
+
+interface SidebarProps {
+  children: React.ReactNode;
+}
+
+const Sidebar: React.FC<SidebarProps> = ({ children }) => {
+  const pathName = usePathname();
+
+  const routes = useMemo(
+    () => [
+      {
+        icon: HiHome,
+        label: "Home",
+        active: pathName !== "/search",
+        href: "/",
+      },
+      {
+        icon: BiSearch,
+        label: "Search",
+        active: pathName === "/search",
+        href: "/search",
+      },
+    ],
+    [pathName]
+  );
+
+  return (
+    <div className="flex h-full">
+      <div className="hidden md:flex flex-col gap-y-2 bg-black h-full w-[300px] p-2">
+        <Box>
+          <div className="flex flex-col gap-y-4 px-5 py-4">
+            {routes.map((route) => (
+              <SidebarItem key={route.label} {...route} />
+            ))}
+          </div>
+        </Box>
+        <Box className="overflow-y-auto h-full">
+          <Library />
+        </Box>
+      </div>
+      <main className="h-full flex-1 overflow-y-auto py-2">{children}</main>
+    </div>
+  );
+};
+
+export default Sidebar;
